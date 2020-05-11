4102cc9a4e7d26f22cad6db5db3b050ce3abd957
Shamai Panchakshar <shamais014@gmail.com>
2020-05-11 14:04:44 +0530

    Learning Variables

--- Hello.py
+++ Hello.py
@@ -5,3 +5,21 @@ print("Hello, Welcome to Python learning")
 x=10
 y=20
 print(x+y)
+
+#Variables
+a= 10
+d= 10.5
+c = 'A'
+s = "Welcome"
+b = True
+
+print(a)
+print(type(a))
+print(d)
+print(type(d))
+print(c)
+print(type(c))
+print(s)
+print(type(s))
+print(b)
+print(type(b))
